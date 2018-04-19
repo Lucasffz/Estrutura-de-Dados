0262eeea8cffa7cedae853b6bcfc60e7ab3b5f20
lucasffz5@hotmail.com <lucasffz5@hotmail.com>
2018-04-19 19:04:32 -0300

    Adicionando Árvore incompleta.

---
+++ Arvore/src/BinaryTree.java
@@ -0,0 +1,9 @@
+public interface BinaryTree extends Tree {
+    public NodeBT left(NodeBT no) throws InvalidPositionException;
+    public NodeBT right(NodeBT no) throws InvalidPositionException;
+    public boolean hasLeft(NodeBT no) throws InvalidPositionException;
+    public boolean hasRight(NodeBT no) throws InvalidPositionException;
+    public NodeBT insertLeft(NodeBT no,Object o) throws InvalidPositionException;
+    public NodeBT insertRight(NodeBT no,Object o) throws InvalidPositionException;
+        
+}
---
+++ Arvore/src/InvalidPositionException.java
@@ -0,0 +1,6 @@
+class InvalidPositionException extends Exception {
+    public InvalidPositionException(String exception) {
+        super(exception);
+    }
+    
+}
